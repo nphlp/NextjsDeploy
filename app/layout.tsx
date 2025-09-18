91d482526391beef17974ca10b712a547e6660e0
nansphilip <nansphilip@gmail.com>
2025-09-18 18:44:15 +0200

    fix: import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/public/globals.css";
 import { combo } from "@/lib/combo";
 import { ReactNode } from "react";
 
