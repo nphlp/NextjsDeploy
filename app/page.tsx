0cd0f20194f97df38d7770c7e5705b42d57244f8
nansphilip <nansphilip@gmail.com>
2025-09-18 15:01:50 +0200

    fix: apos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="min-h-full flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-bold">Je t'aime ❤️</h1>
+            <h1 className="text-2xl font-bold">Je t&apos;aime ❤️</h1>
         </main>
     );
 }
