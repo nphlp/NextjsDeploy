363eb378b81f8684668e8d8c1ee5d8389de1df43
nansphilip <nansphilip@gmail.com>
2025-09-19 20:18:00 +0200

    fix: fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Name from "./name";
+import { TaskFindManyServer } from "@services/server";
 
-export default function Home() {
+export default async function Home() {
+    const taskList = await TaskFindManyServer({});
+
     return (
         <main className="flex min-h-full flex-col items-center justify-center gap-6">
-            <Name />
-            <div>Entrer votre nom 📝</div>
+            <div className="space-y-4">
+                <h1 className="text-2xl font-bold">Ma liste de tâches 📝</h1>
+                <ul className="space-y-2">
+                    {taskList.map((task) => (
+                        <li key={task.id} className="rounded-lg border border-gray-300 px-3 py-1">
+                            {task.title}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </main>
     );
 }
