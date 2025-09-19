331f2b88dda7bb15d5e48013874e38801ee314fc
nansphilip <nansphilip@gmail.com>
2025-09-19 15:00:28 +0200

    feat: yellow

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
         <main className="flex min-h-full flex-col items-center justify-center gap-6">
             <Name />
             <div>Entrer votre nom 📝</div>
+            <div className="rounded-full bg-amber-400 px-3 py-0.5 font-bold text-white">TEST</div>
         </main>
     );
 }
