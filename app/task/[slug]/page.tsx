0cb96c12242004e1b9c260126b573096726cee17
nphlp <128377483+nphlp@users.noreply.github.com>
2025-09-24 23:07:03 +0200

    feat: nextjs ssg build on vps
    * feat: add ssg page
    
    * feat: dockerfile ssg first step
    
    * feat: ssg build works
    
    * feat: ssg local and vps

--- app/task/[slug]/page.tsx
+++ app/task/[slug]/page.tsx
@@ -1,4 +1,15 @@
-import { TaskFindUniqueServer } from "@services/server";
+import Link from "@comps/UI/button/link";
+import { TaskFindManyServer, TaskFindUniqueServer } from "@services/server";
+import { CircleCheckBig, CircleDashed, LoaderCircle } from "lucide-react";
+import { notFound } from "next/navigation";
+
+export const generateStaticParams = async () => {
+    const articles = await TaskFindManyServer({
+        select: { slug: true },
+    });
+
+    return articles.map((article) => ({ slug: article.slug }));
+};
 
 type PageProps = {
     params: Promise<{ slug: string }>;
@@ -10,17 +21,35 @@ export default async function Page(props: PageProps) {
 
     const task = await TaskFindUniqueServer({ where: { slug } });
 
+    if (!task) notFound();
+
     return (
-        <div className="w-full space-y-4 p-4 sm:w-3/4 md:w-2/3">
-            <h1 className="text-2xl font-bold">{slug}</h1>
-            <div>
-                <span>Title: </span>
-                <span>{task?.title}</span>
-            </div>
-            <div>
+        <div className="space-y-4 p-4">
+            <h1 className="text-2xl font-bold">{task.title}</h1>
+            <div className="flex items-center gap-4">
                 <span>Status: </span>
-                <span>{task?.status}</span>
+                <span className="flex items-center gap-2 font-bold">
+                    {task.status === "TODO" && (
+                        <>
+                            <CircleDashed className="size-4" />
+                            <span>À faire</span>
+                        </>
+                    )}
+                    {task.status === "IN_PROGRESS" && (
+                        <>
+                            <LoaderCircle className="size-4" />
+                            <span>En cours</span>
+                        </>
+                    )}
+                    {task.status === "DONE" && (
+                        <>
+                            <CircleCheckBig className="size-4" />
+                            <span>Terminé</span>
+                        </>
+                    )}
+                </span>
             </div>
+            <Link label="Retour à la liste" href="/" />
         </div>
     );
 }
