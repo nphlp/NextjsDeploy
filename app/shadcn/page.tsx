7cb08e4362d4747791ba3f68bc16e70dfb5624b5
nansphilip <nansphilip@gmail.com>
2025-10-07 13:41:23 +0200

    feat: implement shadcn

---
+++ app/shadcn/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@comps/SHADCN/ui/button";
+
+export default function Page() {
+    return (
+        <div className="">
+            <Button>Button</Button>
+        </div>
+    );
+}
